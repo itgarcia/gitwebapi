6320dc34e9295d43aeb9d64e135a57fad22cc0bb
Lenard Garcia <itgarcia.lenard@gmail.com>
2021-08-11 09:24:14 +0800

    Create Web API Day1 Exercise
    First Commit

---
+++ Models/Holiday.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace APIDemo.Models
+{
+    public class Holiday
+    {
+        public string HolidayID { get; set; }
+        public string Holidaydate { get; set; }
+        public string HolidaylocalName { get; set; }
+        public string Holidayname { get; set; }
+        public string HolidaycountryCode { get; set; }
+        public bool Holidayfixed { get; set; }
+        public bool Holidayglobal { get; set; }
+        public string Holidaycounties { get; set; }
+        public int HolidaylaunchYear { get; set; }
+        public string Holidaytype { get; set; }
+    }
+
+}
